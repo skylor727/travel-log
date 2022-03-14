c40ac0e4f8079801df5d1b3e39e186e9177a505f
skylor727 <skylor1997@hotmail.com>
2022-03-14 16:26:36 -0700

    fix send request function

--- src/utilities/photos-api.js
+++ src/utilities/photos-api.js
@@ -1,6 +1,28 @@
-import sendRequest from "./send-request";
 const BASE_URL = "/api/photos";
 
+export default async function sendRequest(
+  url,
+  method = "GET",
+  payload = null,
+  payloadIsFormData = false
+) {
+  // Fetch accepts an options object as the 2nd argument
+  // used to include a data payload, set headers, etc.
+  const options = { method };
+  if (payload) {
+    // If payload is a FormData object, fetch automatically
+    // sets the Content-Type to 'multipart/form-data'
+    options.headers = payloadIsFormData
+      ? {}
+      : { "Content-Type": "application/json" };
+    // If payload is a FormData object, don't stringify it
+    options.body = payloadIsFormData ? payload : JSON.stringify(payload);
+  }
+  const res = await fetch(url, options);
+  // res.ok will be false if the status code set to 4xx in the controller action
+  if (res.ok) return res.json();
+  throw new Error("Bad Request");
+}
 export function apiPostImage(formData) {
   // See refactored sendRequest function that now accepts a 4th arg
   // used to specify that the payload is a FormData object
