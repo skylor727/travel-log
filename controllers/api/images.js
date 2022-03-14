ac2b75736b92a7d70b30e4f1824d3a291e5940d3
skylor727 <skylor1997@hotmail.com>
2022-03-14 14:56:41 -0700

    fix for heroku images #5

--- controllers/api/images.js
+++ controllers/api/images.js
@@ -3,7 +3,7 @@ const uploadFile = require("../../config/s3");
 const upload = async (req, res) => {
   try {
     const result = await uploadFile(req.file);
-    console.warn(result);
+    console.log(result);
     res.send(result.Location);
   } catch (err) {
     res.send(err);
